6ca9ae86ec63ae0329979324aff146b1646cd217
Max Robson <maxwell.robson@gmail.com>
2017-12-21 10:03:58 +0000

    added some prettification

--- app.rb
+++ app.rb
@@ -1,6 +1,5 @@
 require('sinatra')
 require('sinatra/contrib/all')
-# require_relative('models/album.rb')
 require_relative('models/stock.rb')
 require_relative('controllers/albums_controller.rb')
 require_relative('controllers/stocks_controller.rb')
