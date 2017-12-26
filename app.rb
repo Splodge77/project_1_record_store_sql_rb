74c2448b6162b5af95557ef2de24dd5e19419800
Max Robson <maxwell.robson@gmail.com>
2017-12-26 15:02:42 +0100

    added table for new album on home screen

--- app.rb
+++ app.rb
@@ -1,11 +1,13 @@
 require('sinatra')
 require('sinatra/contrib/all')
 require_relative('models/stock.rb')
+require_relative('models/artist.rb')
 require_relative('controllers/albums_controller.rb')
 require_relative('controllers/stocks_controller.rb')
 require_relative('controllers/artists_controller.rb')
 
 get('/') do
   @stocks = Stock.all()
+  @artists = Artist.all()
   erb (:index)
 end
