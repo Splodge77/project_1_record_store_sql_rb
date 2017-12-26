fcf6aa03f9698d3251dafd14671ca4e32bef8cfa
Max Robson <maxwell.robson@gmail.com>
2017-12-26 13:17:20 +0100

    added new artist input to index and put in new flex container next to inventory table

--- db/rec_store.sql
+++ db/rec_store.sql
@@ -11,8 +11,8 @@ CREATE TABLE albums(
   id SERIAL4 PRIMARY KEY,
   title VARCHAR(255),
   artist_id INT4 REFERENCES artists(id) ON DELETE CASCADE,
-  retail int4,
-  wholesale int4
+  retail INT4,
+  wholesale INT4
 );
 
 CREATE TABLE stocks(
